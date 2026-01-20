86fe5f14773f04a8f28fb15da0d257aa5e1e458c
summerqwer999 <summerqwer999@gmail.com>
2026-01-20 19:17:58 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import { TopNavBar } from "components/TopNavBar";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title: "在线简历生成器 | 免费、开源且强大的简历生成器 | OpenResume",
+  title: "在线简历生成器 | 免费、开源且强大的简历生成器 | 简单、快速、有效",
   description:
-    "OpenResume 是一个在线简历生成器，免费、开源且功能强大，帮助你轻松创建专业简历，提升求职竞争力。支持多种模板和个性化设置，无需下载，安全可靠，适合各类求职者使用。",
+    "这是一个在线简历生成器，免费、开源且功能强大，帮助你轻松创建专业简历，提升求职竞争力。支持多种模板和个性化设置，无需下载，安全可靠，适合各类求职者使用。",
 };
 
 export default function RootLayout({
